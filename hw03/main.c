209f397cc347a52c8b64f946e7f66cd88fd42d61
arthur <79771473767@ya.ru>
2025-05-07 14:40:23 +0300

    asm

---
+++ hw03/main.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// empty_str: db 0x0
+char *empty_str = "";
+// int_format: db "%ld ", 0x0
+char *int_format = "%ld";
+// data: dq 4, 8, 15, 16, 23, 42
+long data[] = {4, 8, 15, 16, 23, 42};
+// data_length: equ ($-data) / 8
+int data_length = sizeof(data) / sizeof(data[0]);
+
+// print_int:
+void print_int(const long data) {
+  printf(int_format, data);
+  fflush(stdout);
+}
+
+typedef struct list {
+  long data;
+  struct list *next;
+} list;
+
+// add_element:
+list *add_element(const long data, list *head) {
+  list *l = malloc(sizeof(list));
+  if (l == NULL) {
+    // abort ?? abort()
+    return NULL;
+  }
+  l->data = data;
+  l->next = head;
+  return l;
+}
+
+int main() {
+  list *head = NULL;
+  int i = data_length;
+
+  // adding_loop
+  while (i != 0) {
+    head = add_element(data[i], head);
+    --i;
+  }
+
+  // печать
+  const list *cur = head;
+  while (cur) {
+    printf(int_format, cur->data);
+    cur = cur->next;
+  }
+  puts(empty_str);
+
+  // функция filter(список, акумелятор, предикат)
+  // список
+  cur = head;
+  // акумулятор
+  list *liltered = NULL;
+  while (cur) {
+    // предикат
+    if (cur->data % 2 == 1) {
+      liltered = add_element(cur->data, liltered);
+    }
+    cur = cur->next;
+  }
+
+  // печать
+  while (liltered) {
+    printf(int_format, liltered->data);
+    liltered = liltered->next;
+  }
+  puts(empty_str);
+
+  // без надобности но все же
+  while (head) {
+    const list *tmp = head;
+    free(head);
+    head = tmp->next;
+  }
+}
